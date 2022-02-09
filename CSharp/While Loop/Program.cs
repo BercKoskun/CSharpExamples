615690487252f0fb7a615fb4a06d3b932b4b0a42
Name Surname <test@email.com>
2022-02-09 17:30:07 +0300

    while loop added

---
+++ CSharp/While Loop/Program.cs
@@ -0,0 +1,27 @@
+﻿// 1'den başlayarak Konsoldan girilen sayıya kadar ortalama hesaplayıp Konsola yazdıran program
+Console.Write("Bir Sayı Giriniz: ");
+int sayi=int.Parse(Console.ReadLine());
+int sayac=1;
+int toplam=0;
+while (sayac<=sayi)
+{
+    toplam+= sayac;
+    sayac++;
+}
+Console.WriteLine(toplam/sayi);
+
+// 'a'dan 'z'ye kadar tüm harfleri sırala
+char karakter='a';
+while (karakter < 'z')
+{
+        Console.Write(karakter);
+        karakter++;
+}
+Console.WriteLine("***** Foreach*****");
+
+string[] arabalar= {"BMW","Ford","VW"};
+foreach (var araba in arabalar)
+{
+    Console.WriteLine(araba);
+}
+
