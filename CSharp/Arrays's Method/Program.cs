566df5102e411875c0b4f6596c08122883404e69
Name Surname <test@email.com>
2022-02-10 14:20:47 +0300

    Array Methods

---
+++ CSharp/Arrays's Method/Program.cs
@@ -0,0 +1,49 @@
+﻿
+// Sort -> Dizi Sıralama
+
+int[] sayiDizisi = {23,12,42,54,67};
+
+Console.WriteLine("**** Sırasız Dizi****");
+foreach (var sayi in sayiDizisi)
+    Console.WriteLine(sayi);   
+
+Console.WriteLine("**** Sıralı Dizi****");
+Array.Sort(sayiDizisi);
+
+foreach (var sayi in sayiDizisi)
+    Console.WriteLine(sayi);
+
+// Clear -> Belirlenen elemana 0 atama, temizleme
+Console.WriteLine("**** Array Clear****");
+Array.Clear(sayiDizisi,2,1);
+
+foreach (var sayi in sayiDizisi)
+    Console.WriteLine(sayi);   
+
+// Reverse -> Diziyi ters çevirme
+Console.WriteLine("**** Array Reverse****");
+Array.Reverse(sayiDizisi);
+
+foreach (var sayi in sayiDizisi)
+    Console.WriteLine(sayi);   
+
+//IndexOf 
+Console.WriteLine("**** Array IndexOf ****");
+Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+
+//Resize
+Console.WriteLine("**** Array Resize");
+Array.Resize(ref sayiDizisi,9);
+sayiDizisi[8] = 56;
+
+foreach (var sayi in sayiDizisi)
+    Console.WriteLine(sayi);   
+
+
+
+
+
+
+
+
+
