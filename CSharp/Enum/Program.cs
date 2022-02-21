c228bab44a8ddc59449e022f9a06c22cd9aae487
Name Surname <test@email.com>
2022-02-21 15:30:22 +0300

    .

---
+++ CSharp/Enum/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+Console.WriteLine(Gunler.Pazar);
+Console.WriteLine((int)Gunler.Cumartesi);
+
+
+int sıcaklık=32;
+if(sıcaklık <= (int)HavaDurumu.Normal)
+    Console.WriteLine("Dışarıya çıkmak için havanın ısınmasını bekleyelim");
+else if(sıcaklık >=(int)HavaDurumu.Sıcak)
+    Console.WriteLine("Dışarı çıkmak için fazla sıcak!");
+else if(sıcaklık >= (int)HavaDurumu.Normal && sıcaklık<=(int)HavaDurumu.CokSıcak)
+    Console.WriteLine("Hadi çıkalım!");
+
+
+enum Gunler // default olarak 0'dan başlar.
+{
+    Pazartesi=1,
+    Salı,
+    Çarşamba,
+    Perşembe,
+    Cuma=23,
+    Cumartesi,
+    Pazar
+}
+
+enum HavaDurumu
+{
+    Soguk = 5,
+
+    Normal = 20,
+    Sıcak = 25,
+    CokSıcak = 30
+}
