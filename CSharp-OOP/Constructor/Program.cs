5c9fbe608c2221937a2d4b67d4cf51a1e33a5197
Name Surname <test@email.com>
2022-02-21 12:53:53 +0300

    .

---
+++ CSharp-OOP/Constructor/Program.cs
@@ -0,0 +1,50 @@
+﻿
+Console.WriteLine("*** Çalışan Bir ***");
+
+Calisan calisan1 = new Calisan("Mahmut", "Kuzu", 1231, "IT");
+calisan1.CalisanBilgileri();
+
+Console.WriteLine("*** Çalışan İki ***");
+
+Calisan calisan2 = new Calisan ("Deniz", "Yıldızı", 435,"Satış");
+calisan2.CalisanBilgileri();
+
+Console.WriteLine("*** Çalışan Üç ***");
+Calisan calisan3 = new Calisan("Ahmet", "Aslan");
+calisan3.CalisanBilgileri();
+
+
+class Calisan
+{
+    public string Ad;
+    public string Soyad;
+    public int No;
+    public string Departman;
+
+    public Calisan(string ad, string soyad,int no,string departman)
+    {
+        this.Ad = ad;
+        this.Soyad = soyad;
+        this.No = no;
+        this.Departman = departman;
+    }
+
+    public Calisan(string ad, string soyad)
+    {
+        this.Ad = ad;
+        this.Soyad = soyad;
+    }
+
+
+    public Calisan(){}
+
+    public void CalisanBilgileri()
+    {
+        Console.WriteLine($"Çalışanın Adı: {Ad} ");
+        Console.WriteLine($"Çalışanın Soyadı: {Soyad} ");
+        Console.WriteLine($"Çalışanın Numarası: {No} ");
+        Console.WriteLine($"Çalışanın Departman: {Departman} ");
+    }
+}
+
+
